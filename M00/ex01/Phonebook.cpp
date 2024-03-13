c5493f7c143cd7998b858920fd2e429d87bbe650
ufitzhug <ufitzhug@student.42.fr>
2024-03-13 16:53:18 +0100

    543534

--- M00/ex01/Phonebook.cpp
+++ M00/ex01/Phonebook.cpp
@@ -6,7 +6,7 @@
 /*   By: ufitzhug <ufitzhug@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 14:38:35 by ufitzhug          #+#    #+#             */
-/*   Updated: 2024/03/13 16:33:31 by ufitzhug         ###   ########.fr       */
+/*   Updated: 2024/03/13 16:53:11 by ufitzhug         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,15 @@
 void Print_Data(Phonebook &Obj)
 {
     int index = 0;
-    std::cout << "Input if No of contact (1-8): ";
+    std::cout << "Input if No of contact (0-8): ";
     std::cin >> index;
     std::cout << "Print Data" << std::endl;
     std::cout << "Data_name: " << Obj.A[index].get_first_name() << " Data_family name: " << Obj.A[index].get_last_name() << std::endl;
+    std::cout << "Number: " << Obj.A[index].get_phone_number() << " Secret: " << Obj.A[index].get_darkest_secret() << std::endl;
     return;
 }
     
     
-
 void Enter_Data(Phonebook &Obj, int index)
 {
     std::string first_name;
@@ -35,11 +35,12 @@ void Enter_Data(Phonebook &Obj, int index)
     std::string phone_number;
     std::cout << "Enter your data in following fields (5 lines): ";
     std::cout << "First name, Last name, Nickname, Phone No, Darkest Secret" << std::endl;
-    std::getline (std::cin, first_name);
-    std::getline (std::cin, last_name);
-    std::getline (std::cin, nickname);
-    std::getline (std::cin, phone_number);
-    std::getline (std::cin, darkest_secret);
+    std::getline (std::cin, first_name, '\n');
+    std::getline (std::cin, last_name, '\n');
+    std::getline (std::cin, nickname, '\n');
+    std::getline (std::cin, phone_number, '\n');
+    std::getline (std::cin, darkest_secret, '\n');
+    std::cout << "FN check: " << first_name << std::endl;
     // std::cin >> first_name >> last_name >> nickname >> phone_number >> darkest_secret;
     Obj.A[index].set_data(first_name, last_name, nickname, darkest_secret, phone_number);
     return ;
@@ -71,7 +72,7 @@ int main (void)
         {
             std::cout << "Wrong command. Enter new command" << std::endl;
         }
-        if (index == 3)
+        if (index == 8)
             index = 0;
     }
     return (0);
