7ef190e0614212f2ec33dc58f1ac36396da5b24b
ufitzhug <ufitzhug@student.42.fr>
2024-03-21 15:51:17 +0100

    2334

--- M02/ex00/Fixed.cpp
+++ M02/ex00/Fixed.cpp
@@ -6,8 +6,32 @@
 /*   By: ufitzhug <ufitzhug@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 15:03:31 by ufitzhug          #+#    #+#             */
-/*   Updated: 2024/03/21 15:04:02 by ufitzhug         ###   ########.fr       */
+/*   Updated: 2024/03/21 15:51:14 by ufitzhug         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+    std::cout << "Default constructor called." << std::endl;
+}
+
+Fixed::Fixed (const Fixed &src)
+{
+    std::cout << "Copy constructor called." << std::endl;
+    *this = src;
+}
+
+Fixed& Fixed::operator=(Fixed const & rhs)
+{
+    std::cout << "Copy assignment operator called." << std::endl; 
+    
+}
+
+Fixed 
+
+Fixed::~Fixed()
+{
+    std::cout << "Destructor called." << std::endl;
+}
--- M02/ex00/Fixed.hpp
+++ M02/ex00/Fixed.hpp
@@ -6,12 +6,15 @@
 /*   By: ufitzhug <ufitzhug@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 15:03:35 by ufitzhug          #+#    #+#             */
-/*   Updated: 2024/03/21 15:30:48 by ufitzhug         ###   ########.fr       */
+/*   Updated: 2024/03/21 15:48:29 by ufitzhug         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_HPP
 #define FIXED_HPP
+
+#include <string>
+#include <iostream>
 
 class Fixed
 {
@@ -20,9 +23,9 @@ private:
     static const int nfb;
 public:
     Fixed(/* args */);
+    Fixed (Fixed const & src);
     ~Fixed();
     Fixed& operator=(Fixed const & rhs);
-    Fixed (Fixed const & src);
     int getRawBits( void ) const;
     void setRawBits( int const raw );
 };
