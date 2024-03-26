b17c44e29e3d938fb8f7c16bb16d46ec93218291
ufitzhug <ufitzhug@student.42.fr>
2024-03-26 21:15:03 +0100

    42342

--- M03/ex00/ClapTrap.cpp
+++ M03/ex00/ClapTrap.cpp
@@ -6,7 +6,20 @@
 /*   By: ufitzhug <ufitzhug@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 21:00:26 by ufitzhug          #+#    #+#             */
-/*   Updated: 2024/03/26 21:00:27 by ufitzhug         ###   ########.fr       */
+/*   Updated: 2024/03/26 21:14:58 by ufitzhug         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap(std::string _name) : name(_name), HitPoints(10), EnergyPoints(10), AttackDmg(0)
+{
+    std::cout << "Constructor is called" << std::endl;
+}
+
+ClapTrap::~ClapTrap()
+{
+    std::cout << "Destructor is called" << std::endl;
+}
+
 
--- M03/ex00/ClapTrap.hpp
+++ M03/ex00/ClapTrap.hpp
@@ -6,7 +6,29 @@
 /*   By: ufitzhug <ufitzhug@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 21:00:29 by ufitzhug          #+#    #+#             */
-/*   Updated: 2024/03/26 21:00:30 by ufitzhug         ###   ########.fr       */
+/*   Updated: 2024/03/26 21:11:50 by ufitzhug         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <string>
+#include <iostream>
+
+class ClapTrap
+{
+    private:
+        std::string name;
+        size_t HitPoints;
+        size_t EnergyPoints;
+        size_t AttackDmg;
+    public:
+        ClapTrap(std::string _name);
+        ~ClapTrap();
+        void attack(const std::string& target);
+        void takeDamage(unsigned int amount);
+        void beRepaired(unsigned int amount);
+}
+
+#endif
